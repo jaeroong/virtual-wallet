21b346c2b9de952dca661c474d24346325102b90
David Kim <davidkim024@gmail.com>
2019-08-03 11:08:48 -0400

    setup styles directory

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>new test</p>, document.querySelector("#app"));
+// ReactDOM.render(<p>new test</p>, document.querySelector("#app"));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import IndecisionApp from './components/IndecisionApp';
+// import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+import './styles/styles.scss';
+ReactDOM.render(<p>jun</p>, document.getElementById('app'));
