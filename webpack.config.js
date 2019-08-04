e67c42d48e712efd26b2d8d60f4ee914a3421e40
David Kim <davidkim024@gmail.com>
2019-08-04 18:02:51 -0400

    set bundle.js file minimization false during development

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+  },
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
+  },
 };
