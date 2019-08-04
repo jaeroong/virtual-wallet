6b9143cb0cfb666c90377fcf896e2d014d01b7b3
David Kim <davidkim024@gmail.com>
2019-08-04 18:01:14 -0400

    added React routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// ReactDOM.render(<p>new test</p>, document.querySelector("#app"));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import IndecisionApp from './components/IndecisionApp';
-// import 'normalize.css/normalize.css';
-// import './styles/styles.scss';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 import './styles/styles.scss';
-ReactDOM.render(<p>jun</p>, document.getElementById('app'));
+const HomePage = () => <div>content</div>;
+const CreatePage = () => <div>content dd </div>;
+const UpdatePage = () => <div>update dd </div>;
+const AboutPage = () => <div>about dd </div>;
+const NotFoundPage = () => (
+  <div>
+    404 <Link to="/">Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h3>Header Nav</h3>
+  </header>
+);
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={HomePage} exact={true} />
+      <Route path="/create" component={CreatePage} />
+      <Route path="/update" component={UpdatePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
