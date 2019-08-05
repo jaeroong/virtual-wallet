2aaa29f570c6387a65ec7837410763910b9850bf
David Kim <davidkim024@gmail.com>
2019-08-05 19:43:59 -0400

    setup component folder

---
+++ src/components/HomePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomePage = () => <div>content</div>;
+
+export default HomePage;
---
+++ src/components/RetrievePage.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const RetrievePage = () => <div>retrieve dd </div>;
+
+export default RetrievePage;
